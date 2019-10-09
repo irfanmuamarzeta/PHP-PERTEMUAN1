71d7d6c1e563784d546f72e3f227c09b6a9dd241
irfanmuamarzeta <53320813+irfanmuamarzeta@users.noreply.github.com>
2019-10-09 09:03:15 +0700

    p1b.php

---
+++ p1b.php
@@ -0,0 +1,50 @@
+<?php
+	//echo "hello word !";
+	//echo "<h1> hello Dunia !</h1>";
+
+
+	// VARIABEL
+	//diawali dengan tanda $
+	//nama VARIABEL tidak boleh diawali dengan angka
+	// boleh mengandung angka 
+	
+	//$nama = "irfan muamar zeta";
+	//$pesan = "Halo selamat datang <strong>$nama</strong>";
+	//echo $pesan;
+
+	//OPERATOR
+	//ARITMETIKA
+	//+ - * / %(modulus)
+	
+	//$a=10;
+	//$b=20;
+	//echo $a + $b;
+	
+	//CONCAT/ PENGGABUNGAN
+	//$nama_depan = "KIM";
+	//$nama_belakang ="IRFAN";
+	//echo $nama_depan . " " . $nama_belakang;
+	
+	//ASSIGMENT/PENUGASAN
+	//=, +=, /=, %=, .=
+/*
+	$x = "irfan";
+	$x .= " ";
+	$x .= "mz";
+	echo $x;
+
+	echo "<br>";
+
+	$angka = 10;
+	$angka +=5;
+	$angka +=3;
+	echo $angka;
+
+	/* PERBANDINGAN/COMPARATION
+	==, <=, >=, <, >, !=
+
+	LOGICAL/ LOGIKA
+	&&, !!, !
+
+
+?>
