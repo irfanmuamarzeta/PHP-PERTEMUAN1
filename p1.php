bfbf2d7af5ea10109a8a1b518b70ea37a880c8be
irfanmuamarzeta <53320813+irfanmuamarzeta@users.noreply.github.com>
2019-10-09 09:02:18 +0700

    p1a.php

---
+++ p1.php
@@ -0,0 +1,23 @@
+<?php
+	//echo "hello word !";
+	//echo "<h1> hello Dunia !</h1>";
+
+
+	// VARIABEL
+	//diawali dengan tanda $
+	//nama VARIABEL tidak boleh diawali dengan angka
+	// boleh mengandung angka 
+	
+	//$nama = "irfan muamar zeta";
+	//$pesan = "Halo selamat datang <strong>$nama</strong>";
+	//echo $pesan;
+
+	//OPERATOR
+	//ARITMETIKA
+	//+ - * / %(modulus)
+	
+	//$a=10;
+	//$b=20;
+	//echo $a + $b;
+
+?>
